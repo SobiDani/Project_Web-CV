8e38e14259af39cd0bd4f91e13c98cd8e56fb6f7
adrians_diaz <asanchez@ya-voz.com>
2022-06-13 16:40:41 +0200

    mas subido

---
+++ front/src/app/models/navegador-interface.ts
@@ -0,0 +1,12 @@
+export interface projects {
+    name: string;
+    description: string,
+    URL: string;
+    image: imgPro;
+
+}
+
+export interface imgPro {
+    alt: string;
+    link: string
+}
